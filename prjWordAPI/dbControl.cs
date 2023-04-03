d68aa0201e8e9ed39d4696333383f276f2c70ccb
Liehan Els <liehan13@gmail.com>
2023-04-04 01:48:00 +0200

    Initial sync for wordAPI. -> can retrieve data from URL -> splits data into information -> adds info to objList x> (still) need to hash password? x> still need to successfully insert into dbAdd project files.

--- prjWordAPI/dbControl.cs
+++ prjWordAPI/dbControl.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlClient;
 using System.Net;
 using System.Security.Cryptography.Xml;
+using System.Xml.Linq;
 
 namespace prjWordAPI
 {
@@ -10,7 +11,7 @@ namespace prjWordAPI
         //declarations
         private static string url = "https://wordapidata.000webhostapp.com/";
         private List<string> req = new List<string>();
-        private List<string> userL = new List<string>();
+        private List<dbObject> dbObjects = new List<dbObject>();
         private List<string> afrL = new List<string>();
         private List<string> engL = new List<string>();
         private List<string> xhoL = new List<string>();
@@ -18,7 +19,7 @@ namespace prjWordAPI
         private SqlConnection sqlCon;
         private dbObject obj;
 
-        //get Reece's URL stuff off the website
+        //get Reece's URL stuffFromServer off the website
         public void getURL()
         {
             //url queries
@@ -44,41 +45,39 @@ namespace prjWordAPI
                     string responseFromServer = reader.ReadToEnd();
                     //writes the raw response to console screen for visual verification/error checking
                     Console.WriteLine("Raw response from server\n\n" + responseFromServer + "\n\n-------------<end of raw response>--------------\n");
-                    /* performing a series of string operations to split the raw(r) response >> useful data and information
+                    /* performing a series of (temporarily bad) string operations to split the raw response >> useful data and information
                      * raw data looks like >> [{"Name":"Chris Martin","Password":"football","imageURL":"https:\/\/picsum.photos\/200\/300"},{"Name":.../200\/300"},{...}...]
-                     * removes square brackets */
-                    responseFromServer = responseFromServer.Replace("[", " ");
-                    responseFromServer = responseFromServer.Replace("]", " ");
+                     */
+                    responseFromServer = responseFromServer.Replace("[", "");
+                    responseFromServer = responseFromServer.Replace("]", "");
                     responseFromServer = responseFromServer.Replace("},{", "_");
-                    responseFromServer = responseFromServer.Replace("{", " ");
-                    responseFromServer = responseFromServer.Replace("}", " ");
+                    responseFromServer = responseFromServer.Replace("{", "");
+                    responseFromServer = responseFromServer.Replace("}", "");
+                    responseFromServer = responseFromServer.Replace("\"", "");
                     responseFromServer = responseFromServer.Trim();
                     /*
-                     * data now looks like >> {"Name":"<>","Password":"<>","imageURL":"<>"_"":"","":"","":""_"":"","":"","":""_..._"":"","":"","":""}
-                    writes the modified string because i wanna know what happens
+                     * data now looks like >> Name:<>,Password:<>,imageURL:<>_...
+                    writes the modified string to console because i wanted to know what happened to it
                     */
                     Console.WriteLine("Modified response from server\n\n" + responseFromServer + "\n\n-------------<end of mod response>--------------\n");
                     /*
-                     *
+                     * now splits each record that i seperated by ' _ ' into an array and then further splits it into an object array
                      */
-                    string[] stuff = responseFromServer.Split(new char[] {'_'});
+                    string[] stuffFromServer = responseFromServer.Split(new char[] {'_'});
                     int c = 0;
-                    while (c < stuff.Length)
+                    while (c < stuffFromServer.Length)
                     {
-                        String[] temp = stuff[c].Split(new char[] {','});
-                        int e = 0;
-                        while(e < temp.Length)
-                        {
-                            userL.Add(c + " - " + temp[e].Trim());
-                            e++;
-                        }
+                        String[] temp = stuffFromServer[c].Split(new char[] {','});
+                        string name = (temp[0].Substring(temp[0].IndexOf(':') + 1));
+                        string fname = name.Substring(0, name.IndexOf(" ") + 1).Trim();
+                        string lname = name.Substring(name.IndexOf(" ") + 1).Trim();
+                        string password = (temp[1].Substring(temp[1].IndexOf(':') + 1)).Trim();
+                        string imgURL = (temp[2].Substring(temp[2].IndexOf(':') + 1)).Trim();
+                        //adds each object to array
+                        dbObjects.Add(createDB_Oject(fname, lname, password, imgURL));
+                        // i wanna see if it works
+                        Console.WriteLine("Record : " + c + "->" + dbObjects[c].First_Name + "->" + dbObjects[c].Last_Name + "->" + dbObjects[c].Password_Hash + "->" + dbObjects[c].Image_URL);
                         c++;
-                    }
-                    int i = 0;
-                    while (i < userL.Count)
-                    {
-                        Console.WriteLine("Record :\t" + userL[(i)] + " <end of " + (i) + ">\n" );
-                        i++;
                     }
                 }
             }
